d3aeb9b7dd4a2d7daf3133c84fe636f76c9e61e9
gurdeepsingh2000 <gurdeepsingh20000329@gmail.com>
2021-05-27 14:15:53 +0200

    aggiunto bonus

--- js/my-script.js
+++ js/my-script.js
@@ -2,7 +2,18 @@ var nome = prompt('Inserisci il tuo nome');
 var cognome = prompt('Inserisci il tuo cognome');
 var colore = prompt('Inserisci il tuo colore favorito');
 var x = '21'
+var y = '10'
+var emoji_text = ' Il clown 🤡'
+
 
 var gen_password = nome + cognome + colore + x;
 
 document.getElementById ('password').innerHTML ='La tua password è ' + gen_password;
+
+
+
+document.getElementById ('alias').innerHTML ='Il tuo nuovo alias è ' + nome + emoji_text + ' e hai ' + y + ' anni';
+
+
+
+
